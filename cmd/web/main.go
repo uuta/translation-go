1f2b9c5857b7d67249d2fe6afb245dba60586699
uuta <nightrage.metal66@gmail.com>
2022-10-23 17:27:35 +0900

    Made handleReq()

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -16,22 +16,7 @@ import (
 const serverPort = 3333
 
 func main() {
-	go func() {
-		r := mux.NewRouter()
-		r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-			// an example API handler
-			json.NewEncoder(w).Encode(map[string]bool{"ok": true})
-		})
-		server := http.Server{
-			Addr:    fmt.Sprintf(":%d", serverPort),
-			Handler: r,
-		}
-		if err := server.ListenAndServe(); err != nil {
-			if !errors.Is(err, http.ErrServerClosed) {
-				fmt.Printf("error running http server: %s\n", err)
-			}
-		}
-	}()
+	handleReq()
 
 	time.Sleep(100 * time.Millisecond)
 
@@ -58,4 +43,24 @@ func main() {
 	fmt.Printf("client: status code: %d\n", res.StatusCode)
 
 	io.Copy(out, res.Body)
+}
+
+// handleReq is a function to make WEB server
+func handleReq() {
+	r := mux.NewRouter()
+	// TODO: Fix endpoint
+	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		// an example API handler
+		json.NewEncoder(w).Encode(map[string]bool{"ok": true})
+	})
+	server := http.Server{
+		// TODO: Consider serverPort is properly
+		Addr:    fmt.Sprintf(":%d", serverPort),
+		Handler: r,
+	}
+	if err := server.ListenAndServe(); err != nil {
+		if !errors.Is(err, http.ErrServerClosed) {
+			fmt.Printf("error running http server: %s\n", err)
+		}
+	}
 }
