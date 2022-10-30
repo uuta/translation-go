31aa4f23b1aebed2509302f87154910f7172c7da
uuta <nightrage.metal66@gmail.com>
2022-10-30 15:37:32 +0900

    Just moved the download feature into google.go

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -3,9 +3,7 @@ package main
 import (
 	"errors"
 	"fmt"
-	"io"
 	"net/http"
-	"os"
 	"time"
 
 	"github.com/gorilla/mux"
@@ -18,32 +16,9 @@ const serverPort = 3333
 func main() {
 	handleReq()
 
+	// TODO: Does it need?
 	time.Sleep(100 * time.Millisecond)
 
-	// TODO: make a function that deals with downloading
-	// Make a file
-	out, err := os.Create("translation.mp3")
-	if err != nil {
-		fmt.Printf("error os create: %s\n", err)
-	}
-	defer out.Close()
-
-	url := "https://translate.google.com/translate_tts?ie=UTF-8&client=tw-ob&q=Understand&tl=en&total=1&idx=0"
-	client := &http.Client{}
-	req, _ := http.NewRequest("GET", url, nil)
-	req.Header.Set("Content-Type", "audio/mpeg")
-	req.Header.Set("Content-Disposition", "attachment; filename='translation.mp3'")
-	res, err := client.Do(req)
-	if err != nil {
-		fmt.Printf("error making http request: %s\n", err)
-		os.Exit(1)
-	}
-	defer res.Body.Close()
-
-	fmt.Printf("client: got response!\n")
-	fmt.Printf("client: status code: %d\n", res.StatusCode)
-
-	io.Copy(out, res.Body)
 }
 
 // handleReq is a function to make WEB server
--- internal/google/google.go
+++ internal/google/google.go
@@ -1,10 +1,36 @@
 package google
 
 import (
-	"encoding/json"
+	"fmt"
+	"io"
 	"net/http"
+	"os"
 )
 
 func (h *handler) HandleGet(w http.ResponseWriter, r *http.Request) error {
-	return json.NewEncoder(w).Encode(map[string]bool{"ok": true})
+	// TODO: make a function that deals with downloading
+	// Make a file
+	out, err := os.Create("translation.mp3")
+	if err != nil {
+		return fmt.Errorf("error os create: %s\n", err)
+	}
+	defer out.Close()
+
+	url := "https://translate.google.com/translate_tts?ie=UTF-8&client=tw-ob&q=Understand&tl=en&total=1&idx=0"
+	client := &http.Client{}
+	req, _ := http.NewRequest("GET", url, nil)
+	req.Header.Set("Content-Type", "audio/mpeg")
+	req.Header.Set("Content-Disposition", "attachment; filename='translation.mp3'")
+	res, err := client.Do(req)
+	if err != nil {
+		fmt.Printf("error making http request: %s\n", err)
+		os.Exit(1)
+	}
+	defer res.Body.Close()
+
+	fmt.Printf("client: got response!\n")
+	fmt.Printf("client: status code: %d\n", res.StatusCode)
+
+	io.Copy(out, res.Body)
+	return nil
 }
